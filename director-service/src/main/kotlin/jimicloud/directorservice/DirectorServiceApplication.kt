d3c1727216c6e847083c1425fd9a8d298d602bcc
jimi <jimnorth82@icloud.com>
2023-08-29 09:10:23 -0400

    initial commit

---
+++ director-service/src/main/kotlin/jimicloud/directorservice/DirectorServiceApplication.kt
@@ -0,0 +1,11 @@
+package jimicloud.directorservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DirectorServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DirectorServiceApplication>(*args)
+}
---
+++ director-service/src/test/kotlin/jimicloud/directorservice/DirectorServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package jimicloud.directorservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DirectorServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
