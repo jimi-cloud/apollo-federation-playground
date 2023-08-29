31b5cd7decc9d670a4516d2a59782ae38db02cbe
jimi <jimnorth82@icloud.com>
2023-08-29 09:29:39 -0400

    add dgs to gradle create schema

--- director-service/build.gradle.kts
+++ director-service/build.gradle.kts
@@ -22,13 +22,21 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+@OptIn(kotlin.ExperimentalStdlibApi::class)
+tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
+	generateClient = true
+	packageName = "jimicloud.directorservice.generated"
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
 	}
 }
 
